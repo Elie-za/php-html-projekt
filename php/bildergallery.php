20ca6e00c7f37311bd86b0131a8a484d04ac2e5a
Elie <eili95@web.de>
2021-07-05 22:26:58 +0200

    Navigation gefixt / Style Anpassungen

--- php/bildergallery.php
+++ php/bildergallery.php
@@ -15,10 +15,25 @@ include 'Product.php';
 </head>
 
 <body>
-	<div class="navigation">
-		<a href="../html/index.html">Startseite</a>
-		<a href="bildergallery.php">Bildergallerie</a>
-		<a href="../html/registrierung.html">Registrierung</a>
+<div class="header">
+		<div class="logo">
+		</div>
+		<div class="login">
+			<form action="anmeldung.php" method="post" id="login">
+				<input name="email" type="text">
+				<input name="password" type="text">
+				<input type="submit" value="Login">
+			</form>
+			<div id="session">
+				You are logged in.
+				<button onclick="window.location.href = '../php/logout.php';">Logout</button>
+			</div>
+		</div>
+		<div class="navigation">
+			<a href="../html/index.html">Startseite</a>
+			<a href="bildergallery.php">Bildergallerie</a>
+			<a href="../html/registrierung.html">Registrierung</a>
+		</div>
 	</div>
 	<div class="container">
 		<?php
